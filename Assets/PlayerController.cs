cd96b28c058d23ec071eb5c22fc26def858ab23c
Antti Rautanen <antti.v.j.rautanen@gmail.com>
2021-10-02 09:46:13 +0300

    Player movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Direction direction = Direction.Up;
+
+    private void Update()
     {
-        
+        var nextDirection = GetDirection();
+        if (nextDirection != null)
+        {
+            direction = (Direction)nextDirection;
+        }
+
+        transform.position = GetNextPosition();
     }
 
-    // Update is called once per frame
-    void Update()
+    private static Direction? GetDirection()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            return Direction.Up;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            return Direction.Left;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            return Direction.Down;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            return Direction.Right;
+        }
+
+        return null;
+    }
+
+    private Vector3 GetNextPosition()
+    {
+        var currentPosition = transform.position;
+        return direction switch
+        {
+            Direction.Up => currentPosition + Vector3.up * Time.deltaTime,
+            Direction.Left => currentPosition + Vector3.left * Time.deltaTime,
+            Direction.Down => currentPosition + Vector3.down * Time.deltaTime,
+            Direction.Right => currentPosition + Vector3.right * Time.deltaTime,
+            _ => throw new Exception("Unknown direction " + direction)
+        };
     }
 }

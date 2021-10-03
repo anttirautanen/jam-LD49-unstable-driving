47a78e75c7a12457a1eb0b84e3291f82590df889
Antti Rautanen <antti.v.j.rautanen@gmail.com>
2021-10-03 12:48:26 +0300

    Tilt camera on collision

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,9 +4,22 @@ public class CameraController : MonoBehaviour
 {
     public Transform player;
 
+    private void Start()
+    {
+        PlayerController.OnCollidedWithBuilding += OnCollision;
+    }
+
     private void Update()
     {
         var playerPosition = player.transform.position;
         transform.position = new Vector3(playerPosition.x, playerPosition.y, -10);
+    }
+
+    private static void OnCollision()
+    {
+        if (Camera.main != null)
+        {
+            Camera.main.transform.rotation = Quaternion.Euler(new Vector3(0, 0, Random.value * 10 - 5));
+        }
     }
 }
